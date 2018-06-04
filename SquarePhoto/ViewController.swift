32adc0d8b4da0a1a540bbdb7e671cc6c4ee6195d
guinnesswift <krgoodnews@gmail.com>
2018-06-05 01:32:27 +0900

    Create Project

---
+++ SquarePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquarePhoto
+//
+//  Created by Goodnews on 2018. 6. 5..
+//  Copyright © 2018년 krgoodnews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
